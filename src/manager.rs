cbae22f76e514885014a348b26b796a62d0208c8
Owen Diehl <ow.diehl@gmail.com>
2020-12-18 11:47:20 -0500

    simplifies strategy Signed-off-by: Owen Diehl <ow.diehl@gmail.com>

--- src/manager.rs
+++ src/manager.rs
@@ -40,29 +40,23 @@ async fn reconcile(gs: GratefulSet, ctx: Context<Data>) -> Result<ReconcilerActi
         });
 
     // gs -> pool(immutable-config) -> sts-hash(immutable-config)
-    // Order of operations should be (ScaleDown -> UpdateConfig -> ScaleUp)
 
     // If only the desired pool exists & it has the correct config & replicas, noop.
 
-    // Ensure the desired pool exists with the correct config. If this results in a create (updated pvc definitions, etc), set the desired replicas to 0 and we'll take care of scaling one at a time.
+    // If the desired pool exists but has a different spec (sans replicas), update it and return early. We'll need to wait for the underlying sts to roll to the new spec before continuing.
 
-    // Now we have to handle a few cases:
-    // 1) Only the desired pool exists (change can be applied to underlying pool and managed there)
-    // 2) There are older pool revisions around. First ensure the desired pool has the correct config applied (sans replicas). Once it settles (all relevant pods have been rolled within the sts), we can handle scaling as per usual.
+    // If the desired pool does not exist, we'll want to create it starting at 0 replicas.
 
-    // If replicas across all pools < desired replicas,
-    // add one to desired pool (ScaleUp).
+    // Now we have to handle a few cases for scaling:
+    // Order of operations should be (ScaleDown -> ScaleUp)
 
     // If replicas across all pools >= desired replicas,
     // remove one from the most out of date pool (ScaleDown). This
     // mimics the statefulset rollout semantics where
     // one is removed before adding a new revision replica.
 
-    // pod|replica change -> update pool definition
-    // What if has an old pool lying around from previous definition?
-    // Should take down old pool first
-    // If not?
-    // Update the new pool spec and let it handle rollout.
+    // If replicas across all pools < desired replicas,
+    // add one to desired pool (ScaleUp).
 
     Ok(ReconcilerAction {
         // try again in 5min
